3008f2edc34c8532ec8d5cf4d501acc52d9da382
hakanyorukk <hakanyorukomer@hotmail.com>
2024-10-09 00:46:16 +0300

    Implemented question pages

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,9 +1,34 @@
 import { useParams } from "react-router-dom";
+import { questions } from "../utils/utils";
 
 function Questions() {
   const { questionId } = useParams();
   console.log(questionId);
-  return <section className="question-section">adsa</section>;
+  return (
+    <section className="question-section">
+      <main className="question">
+        <div className="question-box">
+          <h1 className="question-box__title">
+            {questions[questionId - 1].question}
+          </h1>
+          <div className="question-box__answers">
+            {questions[questionId - 1].answers.map((answer, index) => (
+              <p key={index} className="question-box__answer">
+                {answer}
+              </p>
+            ))}
+          </div>
+          <div className="question-box__buttons">
+            <button className="question-box__buttons__back">Back</button>
+            <button className="question-box__buttons__forward">
+              Next question <img src="/img/arrow.svg" />
+            </button>
+          </div>
+        </div>
+        <div className="question-progressbar"></div>
+      </main>
+    </section>
+  );
 }
 
 export default Questions;
---
+++ src/utils/utils.jsx
@@ -0,0 +1,40 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What's your hair type or texture?",
+    answers: ["Straight", "Curly", "Wavy", "Fine"],
+  },
+  {
+    id: 2,
+    question: "How often do you wash your hair?",
+    answers: [
+      "Daily",
+      "Every other day",
+      "Twice a week",
+      "Once a week",
+      "Once every two weeks",
+    ],
+  },
+  {
+    id: 3,
+    question: "What benefit do you look for in your hair products?",
+    answers: [
+      "Anti-breakage",
+      "Hydration",
+      "Soothing dry scalp",
+      "Repairs the appearance of damaged hair",
+      "Volume",
+      "Curl and coil enhancing",
+    ],
+  },
+  {
+    id: 4,
+    question: "Is there anything troubling you about your hair?",
+    answers: ["Breakage", "Frizz", "Scalp dryness", "Damage", "Tangling"],
+  },
+  {
+    id: 5,
+    question: "What is your natural hair color(s) today?",
+    answers: ["Black", "Brown", "Blonde", "Red/Orange", "Silver/Grey"],
+  },
+];
